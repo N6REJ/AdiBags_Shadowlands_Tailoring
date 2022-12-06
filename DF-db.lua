87d8f8a54f5032b379a725d3870d89ce789a09f0
Bear <programming@hallhome.us>
2022-12-06 05:43:54 -0600

    more zone reagents

--- DF-db.lua
+++ DF-db.lua
@@ -86,6 +86,10 @@ db.Filters = {
 			[202122] = true, -- Primal Chaos Cluster
 			[200686] = true, -- Primal Focus
 			[190456] = true, -- Artisan's Mettle
+			[201405] = true, -- Tuft of Primal Wool -- Used by many professions
+			[201406] = true, -- Glowing Titan Orb   -- Used by many professions
+			[190453] = true, -- Spark of Ingenuity
+			[199197] = true, -- Bottled Essence
 		}
 	}
 }
